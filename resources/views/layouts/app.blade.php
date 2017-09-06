11a05eb6b41ed645402cf8a6abdc407e0bfbee6d
samuelthor <Vistaediting@gmail.com>
2017-09-06 15:11:10 +0000

    skilverk1 update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,18 +32,13 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
-                    <a class="navbar-brand" href="{{ url('/threads') }}">
-                      Threads
-                    </a>
-
-                    <a class="navbar-brand" href="{{ url('/threads/create') }}">
-                      New Thread
-                    </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
+                        <li><a href="/threads">threads</a></li>
+                        <li><a href="/threads/create">Create new thread</a></li>
                         &nbsp;
                     </ul>
 
