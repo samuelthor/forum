0eae82951c1caa85377c64623f8ab7ffef144271
samuelthor <Vistaediting@gmail.com>
2017-09-13 10:41:07 +0000

    Timaverk4

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,15 @@ Route::get('/threads/create', function() {
 	return view('threads.create');
 });
 
-Route::get('/', function(){
-	$name = 'Samuel';
-	return view('messages.hello', compact('name'));
-});
+// Route::get('/', function(){
+// 	$name = 'Samuel';
+// 	return view('messages.hello', compact('name'));
+// });
 
 Route::get('/greeting/{id}', function($id){
 	$greetings = ['Polski','Russki','Icelandic','Svergie','English'];
 
 	return view('messages.greeting', ['greeting' =>$greetings[$id]]);
 })->where(['id' => '[0-4]']);
+
+Route::get('/', 'GreetingController@show');
